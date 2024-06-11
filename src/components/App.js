3045b8abfc54dd6602d0d40dd3ac15289e2be0aa
smith <smithbabawande@gmail.com>
2024-06-11 11:44:51 +0100

    second commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import '../components/App.css';
+import { Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+    <Routes>
+      
+    </Routes>
+
+    </>
+  );
+}
+
+export default App;
